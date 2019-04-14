f31d5de38e69261820d414de4652ba813cb7fdba
叶昕 <a649414476@gmail.com>
2019-04-14 18:05:17 +0800

    :sparkles: config

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME', '649414476@qq.com')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <649414476@qq.com>'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[No.996]'
+    FLASKY_MAIL_SENDER = 'No.996 Team <649414476@qq.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN', '649414476@qq.com')
 
     @staticmethod
